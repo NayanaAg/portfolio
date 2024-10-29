7ec481396c45944f1fead51f23327abab90b6c40
NayanaAg <101566994@georgebrown.ca>
2024-10-29 00:17:55 +0000

    create and style nav component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import GlobalNav from "./components/nav";
 
 export default function Page() {
     return <>
         <div id="container">
+        <GlobalNav />
             <div className="row">
                 <div className="col-md-10 offset-md-1">
+                    
+
                     <h1>Welcome to my website!</h1>
                     <p>This is a test to see whether I can make my way around this website with React</p>
 

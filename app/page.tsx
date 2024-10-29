d3acd9d7662391fe0b270f12b924e4f27114faed
NayanaAg <101566994@georgebrown.ca>
2024-10-29 19:37:57 +0000

    importing media and images

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             <main>
                 <div className="row" id="hero-banner">
                     <div className="col-md-5 offset-md-1">
-
+                        <img src="hero-banner.jpg" alt="picture of me" />
                     </div>
                     <div className="col-md-5">
                         <h1>welcome to my portfolio.</h1>

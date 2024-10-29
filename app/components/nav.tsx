7b240a2036b55cd899af940c324dce690bd391bf
NayanaAg <101566994@georgebrown.ca>
2024-10-29 00:25:34 +0000

    created pages for main components and pages

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,16 +2,20 @@ import React from "react";
 
 export default function GlobalNav() {
     return <>
-        <header>
-            <div id="global-nav">
-                <ul>
-                    <li><a href="/">home</a></li>
-                    <li><a href="/projects">my work</a></li>
-                    <li><a href="/hobbies">hobbies</a></li>
-                    <li><a href="/contact">contact</a></li>
-                    <li><a href="#">resume</a></li>
-                </ul>
+        <div className="row g-0">
+            <div className="col-12">
+                <header>
+                    <div id="global-nav">
+                        <ul>
+                            <li><a href="/">home</a></li>
+                            <li><a href="/projects">my work</a></li>
+                            <li><a href="/hobbies">hobbies</a></li>
+                            <li><a href="/contact">contact</a></li>
+                            <li><a href="#">resume</a></li>
+                        </ul>
+                    </div>
+                </header>
             </div>
-        </header>
+        </div>
     </>
 }

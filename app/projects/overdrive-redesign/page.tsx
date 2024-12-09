7b04aaf7c62f7994424b52d8fc3f27bd07a4a274
NayanaAg <101566994@georgebrown.ca>
2024-12-09 19:29:10 +0000

    tpl, portfolio pages finished

--- app/projects/overdrive-redesign/page.tsx
+++ app/projects/overdrive-redesign/page.tsx
@@ -36,16 +36,24 @@ export default function Page() {
                     <div className="row">
                         <div className="col-8 offset-2">
                             <h2>The brief</h2>
-                            <p></p>
+                            <p>Identify pain points in the user experience of an existing website. Suggest redesigns for each pain point on both desktop and mobile, being mindful of avoiding changes for the sake of change.</p>
 
                             <h2>Process</h2>
-                            <p></p>
+                            <p>In my redesign, I focused on restructuring information on the website. I wanted to address the user heuristic principles of “consistency and standards” and “recognition versus recall”. I reorganized menus and content on the page to help prioritize discovery of new content that a user may be looking for based on genre, age group, or popularity.</p>
+
+                            <p>I made almost no changes to the branding or visual styles when redesigning the pages. I think the TPL design is simple and unobtrusive but also very good for contrast, since they focus on using black and white with a neutral shade of blue for actions. I think accessibility should be one of the core considerations of an institution like the library because of the focus on distributing content to all people equally. I also wanted to avoid change for the sake of change, so I kept my designs focused on making books and other digital content easier to find. </p>
 
                             <h2>Notable elements</h2>
-                            <p></p>
+                            <ul>
+                                <li>Brand new navigation menu with logical categorization of books</li>
+                                <li>Complex footer which preserves the Overdrive branding while providing content related to the libarry website</li>
+                                <li>Suggestion for a rehaul of the "help and guidelines" pages, which are notoriously difficult to navigate</li>
+                            </ul>
 
                             <h2>Project takeaways</h2>
-                            <p></p>
+                            <p>I learned that websites with many moving parts benefit from extra time dedicated to sitemapping. I made 3-4 versions of my sitemap every time I found new menus or sections of the website before settling on the final navigation bar.</p>
+
+                            <p>This project also showed me the value of universal design concepts like the hamburger mobile menu and square tiles for audiobooks. There's a reason everyone uses these ideas in their desigs, and it's better to conform and make life easier for the user than try to be inventive and lead to confusion.</p>
                         </div>
                     </div>
                 </section>
--- app/projects/portfolio-process/page.tsx
+++ app/projects/portfolio-process/page.tsx
@@ -36,16 +36,20 @@ export default function Page() {
                     <div className="row">
                         <div className="col-8 offset-2">
                             <h2>The brief</h2>
-                            <p></p>
+                            <p>Make a portfolio to show your relevant projects and work experience.</p>
 
                             <h2>Process</h2>
-                            <p></p>
+                            <p>I worked on this portfolio for a quite a while. I wanted it to have not only my work experience and resume, but also show my personality a bit with the colours, hobby page, and language. I followed the tradition design process of sitemapping, low fidelity wireframes, mockups, and then finally the coding. I find that the best way to organize my thoughts and strengthen the final product without wasting time writing and rewriting pages of code.</p>
 
                             <h2>Notable elements</h2>
-                            <p></p>
+                            <ul>
+                                <li>Project tags to filter my work experience based on the user's requirements</li>
+                                <li>Carousels and sliders to show visuals for each project</li>
+                                <li>Complex React components to manage project tiles and all relevant information within them</li>
+                            </ul>
 
                             <h2>Project takeaways</h2>
-                            <p></p>
+                            <p>I'm really proud of how this portfolio is turning out, even though I can spot lots of things to fix and fiddle with (I'll probably never stop!). I learned a lot about React, use states, array mapping and filtering, and component props because I forced myself to design complex features as a way of improving my coding skills. I understand the potential of React and Javascript a lot better now, and I'm looking forward to develop projects with data storage and user interaction in the future!</p>
                         </div>
                     </div>
                 </section>

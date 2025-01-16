3fc82c9e5e8f06da9564456c488409647a4605c1
Nayana Agrawal <101566994@georgebrown.ca>
2025-01-16 17:54:24 +0000

    add content to zucora website page

--- app/projects/zucora-website/page.tsx
+++ app/projects/zucora-website/page.tsx
@@ -37,16 +37,25 @@ export default function Page() {
                     <div className="row">
                         <div className="col-8 offset-2">
                             <h2>The brief</h2>
-                            <p></p>
+                            <p>Rehaul the Zucora website from the ground up with new branding, better content strategy, and better visual assets and layout. Also develop a new Content Management System (CMS) to help maintain the website in the future.</p>
 
                             <h2>Process</h2>
-                            <p></p>
+                            <p>We worked with an external web development agency for this project, which was a new experience for me. There were a lot of learning opportunities and challenges: communicating between teams, organizing deadlines, keeping different people involved in different aspects of the project, etc.
+                            </p>
+
+                            <p>The biggest thing I learned about though was scope creep. Since we were working with an agency, we had to finalize our feature list and requirements before ever seeing wireframes or prototypes, which was a new experience for all of us. Inevitably, there were things that didn't work exactly how we wanted, certain important features that got lost in translation, and others that were trimmed so we could deliver the minimum viable product by our deadline.</p>
+
+                            <p>I'm glad I was involved in such a massive project though, because I was able to learn a lot about good working methodology, testing pages designed and developed by others, and crystalizing your requirements clearly and directly to make sure the planning is sufficient to prepare the team for success.</p>
 
                             <h2>Notable elements</h2>
-                            <p></p>
-
-                            <h2>Project takeaways</h2>
-                            <p></p>
+                            <ul>
+                                <li>A custom CMS with coupon code and promotion functionality, product SKU management, and a page 
+                                     builder with a collection of components and elements
+                                </li>
+                                <li>New, fresh branding with brighter colors, a direct and easy-to-understand brand voice, and organized product array to support retailers' needs.</li>
+                                <li>A partner dashboard and login to place orders online, manage their account, keep track of shipping, and receive unique customized promotions and coupons through a customer loyalty program.</li>
+                                <li>A robust plan for implementing phase II features including an email builder, French SEO and meta data, detailed customization within page builder blocks, and more!</li>
+                            </ul>
                         </div>
                     </div>
                 </section>
